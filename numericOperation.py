18d6f0dc3c4cf447ff437743cb271a14d6f140a6
Yongjia Li <yoli@tcd.ie>
2022-09-21 17:27:43 +0100

    test

---
+++ numericOperation.py
@@ -0,0 +1,2 @@
+def multiply_op(x, y):
+    return x*y
---
+++ testing.py
@@ -0,0 +1,6 @@
+from matplotlib import test
+from numericOperation import*
+def testing_multiply():
+    assert (multiply_op(2,3) == 6)
+
+testing_multiply()
